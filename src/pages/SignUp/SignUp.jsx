e25e3bbe8480f94d8d2842578539cd21c49fde22
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-05-30 21:15:45 +0600

    added loading spinner while user is signing in

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -40,6 +40,16 @@ const SignUp = () => {
       setLoading(false);
     }
   };
+
+  const handleGoogleSignUp = async () => {
+    setLoading(true);
+    try {
+      await signInWithGoogle();
+      navigate("/");
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900">
@@ -111,11 +121,15 @@ const SignUp = () => {
           <p className="px-3 text-sm dark:text-gray-400">Signup with social accounts</p>
           <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
         </div>
-        <div className="flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer">
+        <button
+          disabled={loading}
+          onClick={handleGoogleSignUp}
+          className="disabled:cursor-not-allowed flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer"
+        >
           <FcGoogle size={32} />
 
           <p>Continue with Google</p>
-        </div>
+        </button>
         <p className="px-6 text-sm text-center text-gray-400">
           Already have an account?{" "}
           <Link to="/login" className="hover:underline hover:text-rose-500 text-gray-600">
