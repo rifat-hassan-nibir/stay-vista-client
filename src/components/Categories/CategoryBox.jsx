d512ff5321bb2c2ce444c8d22508fde2d57c4888
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-05-30 10:10:05 +0600

    added dynamic query using react query package to load different category data from server

--- src/components/Categories/CategoryBox.jsx
+++ src/components/Categories/CategoryBox.jsx
@@ -1,8 +1,24 @@
 import PropTypes from "prop-types";
+import queryString from "query-string";
+import { useNavigate } from "react-router-dom";
 
 const CategoryBox = ({ label, icon: Icon }) => {
+  const navigate = useNavigate();
+
+  const handleQuery = () => {
+    let currentQuery = { category: label };
+    const url = queryString.stringifyUrl({
+      url: "/",
+      query: currentQuery,
+    });
+    navigate(url);
+  };
+
   return (
-    <div className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer`}>
+    <div
+      onClick={() => handleQuery()}
+      className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer`}
+    >
       <Icon size={26} />
       <div className="text-sm font-medium">{label}</div>
     </div>
