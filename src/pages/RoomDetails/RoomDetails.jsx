1eebe1a96df0c65f583e755bf2f0bad5d15eaeb1
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-05-30 08:52:13 +0600

    used axios secure

--- src/pages/RoomDetails/RoomDetails.jsx
+++ src/pages/RoomDetails/RoomDetails.jsx
@@ -41,30 +41,12 @@ const RoomDetails = () => {
             {/* Room Info */}
             <div className="col-span-4 flex flex-col gap-8">
               <div className="flex flex-col gap-2">
-                <div
-                  className="
-                text-xl 
-                font-semibold 
-                flex 
-                flex-row 
-                items-center
-                gap-2
-              "
-                >
+                <div className="text-xl font-semibold flex flex-row items-center gap-2">
                   <div>Hosted by {room?.host?.name}</div>
 
                   <img className="rounded-full" height="30" width="30" alt="Avatar" src={room?.host?.image} />
                 </div>
-                <div
-                  className="
-                flex 
-                flex-row 
-                items-center 
-                gap-4 
-                font-light
-                text-neutral-500
-              "
-                >
+                <div className="flex flex-row items-center gap-4 font-lighttext-neutral-500">
                   <div>{room?.guests} guests</div>
                   <div>{room?.bedrooms} rooms</div>
                   <div>{room?.bathrooms} bathrooms</div>
@@ -72,12 +54,7 @@ const RoomDetails = () => {
               </div>
 
               <hr />
-              <div
-                className="
-          text-lg font-light text-neutral-500"
-              >
-                {room?.description}
-              </div>
+              <div className="text-lg font-light text-neutral-500">{room?.description}</div>
               <hr />
             </div>
 
