65396695f787f4974f779258a48ed5e2a24c4ff0
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-05-30 12:06:56 +0600

    get category data using query string in the url

--- src/components/Categories/CategoryBox.jsx
+++ src/components/Categories/CategoryBox.jsx
@@ -1,23 +1,31 @@
 import PropTypes from "prop-types";
 import queryString from "query-string";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const CategoryBox = ({ label, icon: Icon }) => {
+  const [params, setParams] = useSearchParams();
+  const category = params.get("category");
   const navigate = useNavigate();
 
   const handleQuery = () => {
+    // 1. create query string
     let currentQuery = { category: label };
     const url = queryString.stringifyUrl({
       url: "/",
       query: currentQuery,
     });
+    // url output --> /?category=label
+
+    // 2. set query string to url
     navigate(url);
   };
 
   return (
     <div
       onClick={() => handleQuery()}
-      className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer`}
+      className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer ${
+        category === label && "border-b-neutral-800 text-neutral-900"
+      }`}
     >
       <Icon size={26} />
       <div className="text-sm font-medium">{label}</div>
