8a9281aca37ad6480b380389720579d64f8f2069
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-05-30 06:09:14 +0600

    added tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,23 @@
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import AuthProvider from './providers/AuthProvider'
-import { router } from './routes/Routes'
-import { Toaster } from 'react-hot-toast'
-import 'react-date-range/dist/styles.css'
-import 'react-date-range/dist/theme/default.css'
-import { HelmetProvider } from 'react-helmet-async'
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import AuthProvider from "./providers/AuthProvider";
+import { router } from "./routes/Routes";
+import { Toaster } from "react-hot-toast";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { HelmetProvider } from "react-helmet-async";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <HelmetProvider>
-    <AuthProvider>
-      <RouterProvider router={router} />
-      <Toaster />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+        <Toaster />
+      </AuthProvider>
+    </QueryClientProvider>
   </HelmetProvider>
-)
+);
